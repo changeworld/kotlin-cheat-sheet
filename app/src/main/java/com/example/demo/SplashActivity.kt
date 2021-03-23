505da27bc9f3ef1bd1798ad1584fbebaa3e7bf8c
Takashi Takebayashi <changesworlds@gmail.com>
2021-03-23 12:07:52 +0900

    Fix typo

--- app/src/main/java/com/example/demo/SplashActivity.kt
+++ app/src/main/java/com/example/demo/SplashActivity.kt
@@ -4,9 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class SplashActivity : AppCompatActivity() {
     private val handler = Handler()
