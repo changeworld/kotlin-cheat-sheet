f61279a95dedea02920be28c3042dd5adacf84bd
Takashi Takebayashi <changesworlds@gmail.com>
2021-04-14 22:44:10 +0900

    Add activity transition
    - FuelDownloadActivity -> MainActivity

--- app/src/main/java/com/example/demo/FuelDownloadActivity.kt
+++ app/src/main/java/com/example/demo/FuelDownloadActivity.kt
@@ -1,11 +1,20 @@
 package com.example.demo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class FuelDownloadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fuel_download)
+        val back: TextView = findViewById(R.id.back)
+        back.setOnClickListener {
+            val intent = Intent(application, MainActivity::class.java)
+            startActivity(intent)
+            // Finish　FuelDownloadActivity
+            finish()
+        }
     }
 }
